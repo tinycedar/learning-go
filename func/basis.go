1b03146e07d95c82a6108aa4f38ed52b52ac8619
Daniel Lin <qienlin@outlook.com>
2016-01-17 19:46:45 +0800

    Modify comments

--- func/basis.go
+++ func/basis.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 )
 
-// 1, 如果接收者值未在方法体内使用，可以省略方法接收者标识符
-// 2, 可变长参数的使用
-// 3, 多返回值的使用
-// 4, 返回值的声明和使用
+// 1, 可变长参数的使用
+// 2, 多返回值的使用
+// 3, 返回值的声明和使用
 func hello(prefix string, names ...string) (result string, err error) {
 	result = prefix
 	length := len(names)
